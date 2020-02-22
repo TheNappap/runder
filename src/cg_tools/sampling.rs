1e3e60cdc19541ee85218f6110338392abfc27bc
TheNappap <napoleonpapiertjes@gmail.com>
2020-02-22 12:27:33 +0100

    Added hemispherical sampling
    Hemispherical sampling is consine weighted, for importance sampling

--- src/cg_tools/sampling.rs
+++ src/cg_tools/sampling.rs
@@ -33,17 +33,33 @@ impl SamplingTechnique {
                 let v = (height/sqrt_amt as f64)*(j as f64 + 0.5);
                 (u,v)
             }).collect(),
-            SamplingTechnique::Random{..} => (0..(sqrt_amt*sqrt_amt)).map(|_| {
-                let u = width*rand::random::<f64>();
-                let v = height*rand::random::<f64>();
-                (u,v)
-            }).collect(),
+            SamplingTechnique::Random{..} => self.sample_rect_random(width, height, sqrt_amt*sqrt_amt),
             SamplingTechnique::Stratified{..} => iproduct!(0..sqrt_amt,0..sqrt_amt).map(|(i,j)|{
                 let u = (width/sqrt_amt as f64)*(i as f64 + rand::random::<f64>());
                 let v = (height/sqrt_amt as f64)*(j as f64 + rand::random::<f64>());
                 (u,v)
             }).collect()
         }
+    }
 
+    fn sample_rect_random(&self, width: f64, height: f64, sample_amt: i32) -> Vec<(f64,f64)> {
+        (0..sample_amt).map(|_| {
+            let u = width*rand::random::<f64>();
+            let v = height*rand::random::<f64>();
+            (u,v)
+        }).collect()
+    }
+
+    pub fn sample_hemisphere(&self, branching_factor: i32) -> Vec<(f64,f64,f64)> {
+        //https://cg.informatik.uni-freiburg.de/course_notes/graphics2_08_renderingEquation.pdf
+        self.sample_rect_random(1., 1., branching_factor).iter().map(|(u,v)| {
+            let cos_theta = (1. - u).sqrt();
+            let sin_theta = u.sqrt();
+            let phi = 2. * std::f64::consts::PI * v;
+
+            let x = sin_theta * phi.cos();
+            let z = sin_theta * phi.sin();
+            (x,cos_theta,z)
+        }).collect()
     }
 }
